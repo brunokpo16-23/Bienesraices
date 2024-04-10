ea16284c8906f42cc180e249555333c90da995fa
bruno spinsanti <159073591+brunokpo16-23@users.noreply.github.com>
2024-04-10 20:20:22 -0300

    .

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    eventListeners();
 
-    darkMode();
-});
+document.addEventListener('DOMContentLoaded', function () {
+  responsiveMenu();
+  darkMode();
+})
+ 
+const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)'); 
 
 function darkMode() {
-    const botonDarkMode = document.querySelector(".dark-mode-boton");
-
-    botonDarkMode.addEventListener("click", function() {
-document.body.classList.toggle("dark-mode");
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+ 
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+ 
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+ 
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+ 
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
     });
 }
+
 
 
 
